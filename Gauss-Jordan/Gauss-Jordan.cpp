72a2093827cd573dc2016bf6e53645429aca91d4
davgo0103 <davvgo@gmail.com>
2022-03-01 13:34:49 +0800

    加入專案檔案。

---
+++ Gauss-Jordan/Gauss-Jordan.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    //int m, n;
+    //double **matrix;
+    //cout << " 請輸入矩陣大小(m*n):\nm=";
+    //cin >> m;
+    //cout << "n=";
+    //cin >> n;
+    //matrix = (double**)malloc(m * sizeof(double*));
+    //cout << "請輸入矩陣:" << endl;
+    //for (int i = 0; i < m; i++) 
+    //{
+    //    matrix[i] = (double*)malloc(n * sizeof(double));
+    //    for (int j = 0; j < n; j++) 
+    //    {
+    //        cin >> matrix[i][j];
+    //    }
+    //}
+
+
+    //for (int i = 0; i < m; i++) {
+    //    for(int j = 0; j < n; j++) {
+    //        cout << matrix[i][j] << " ";
+    //    }
+    //    cout << endl;
+    //}
+    
+}
