14ae31849b7b92e5262a474948de3bbf5472bec6
davgo0103 <davvgo@gmail.com>
2022-03-03 17:35:42 +0800

    完成

--- Gauss-Jordan/Gauss-Jordan.cpp
+++ Gauss-Jordan/Gauss-Jordan.cpp
@@ -1,13 +1,10 @@
 ﻿#include <iostream>
-
-
+#include <iomanip>
 using namespace std;
-
-void swap(double **p,int *tx, int *ty,int *m,int *n) {
-    
+void swap(double** p, int* tx, int* ty, int* m, int* n) {
     double tmp;
     int tmpx = *tx, tmpy = *ty;
-    sw:
+sw:
     for (int i = tmpy; i < *m; i++) {
         if (p[i][tmpx] != 0) {
             for (int s = tmpx; s < *n; s++) {
@@ -23,12 +20,35 @@ void swap(double **p,int *tx, int *ty,int *m,int *n) {
     if (tmpx != *n && tmpy != *m) {
         goto sw;
     }
-    
+}
+void up(double** p, int* tx, int* ty, int* m, int* n) {
+    int tmpx = *tx, tmpy = *ty;
+    double num;
+    for (int i = tmpy - 1; i >= 0; i--) {
+        num = p[i][tmpx];
+        if (num != 0) {
+            for (int j = tmpx; j < *n; j++) {
+                p[i][j] = p[i][j] + (p[tmpy][j] * -1 * num);
+            }
+        }
+    }
+}
+void down(double** p, int* tx, int* ty, int* m, int* n) {
+    int tmpx = *tx, tmpy = *ty;
+    double num;
+    for (int i = tmpy + 1; i < *m; i++) {
+        num = p[i][tmpx];
+        if (num != 0) {
+            for (int j = tmpx; j < *n; j++) {
+                p[i][j] = p[i][j] + (p[tmpy][j] * -1 * num);
+            }
+        }
+    }
 }
 void operation(double** p, int* tx, int* ty, int* m, int* n) {
-    int tmpy = *ty; 
+    int tmpy = *ty;
     double leader = p[tmpy][*tx];
-    if (leader != 0) { 
+    if (leader != 0) {
         for (int i = *tx; i < *n; i++) { //取得前導1
             if (leader != 1) {
                 p[tmpy][i] = p[tmpy][i] / leader;
@@ -37,69 +57,45 @@ void operation(double** p, int* tx, int* ty, int* m, int* n) {
                 break;
             }
         }
-        up(p, tx, ty, m, n);
-        down(p, tx, ty, m, n);
+        up(p, tx, ty, m, n);down(p, tx, ty, m, n);
     }
-    
-    *ty = *ty + 1;
+    *ty = *ty + 1;*tx = *tx + 1;
 }
-
-
-void up(double** p, int* tx, int* ty, int* m, int* n) {
-    int tmpx = *tx, tmpy = *ty;
-    
-}
-
-void down(double** p, int* tx, int* ty, int* m, int* n) {
-    int tmpx = *tx, tmpy = *ty;
-}
-
 void print(double** p, int m, int n) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            cout << p[i][j] << " ";
+            cout << setw(2) << p[i][j] << " ";
         }
         cout << endl;
     }
 }
-
-
 int main()
 {
-    int m, n,x=0,y=0;
-    double **matrix;
-
-
-    cout << " 請輸入矩陣大小(m*n):\nm=";
+    int m, n, x = 0, y = 0;
+    double** matrix;
+    cout << "Please enter the matrix size(m*n):\nm=";
     cin >> m;
     cout << "n=";
     cin >> n;
     matrix = (double**)malloc(m * sizeof(double*));
-    cout << "請輸入矩陣:" << endl;
-    for (int i = 0; i < m; i++) 
+    cout << "Please enter a matrix:" << endl;
+    for (int i = 0; i < m; i++)
     {
         matrix[i] = (double*)malloc(n * sizeof(double));
-        for (int j = 0; j < n; j++) 
+        for (int j = 0; j < n; j++)
         {
             cin >> matrix[i][j];
         }
     }
-
-
     while (true)
     {
         if (matrix[y][x] == 0 && x < n && y < m) { //找出非零整數
             swap(*&matrix, &x, &y, &m, &n);
         }
         operation(*&matrix, &x, &y, &m, &n);
-
-        
-
-            if (x + 1 > n || y + 1 > m) {
+        if (x + 1 > n || y + 1 > m) {
             break;
         }
     }
-
     print(*&matrix, m, n);
-    cout << x << " " << y;
 }
